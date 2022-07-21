902d5e70540aeea03b1020efa13b196c3878cc7a
finansist138 <103438132+finansist138@users.noreply.github.com>
2022-07-21 21:35:04 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+
+   private boolean active;
+   private int status;
+   private String message;
+
+   public Error(boolean active, int status, String message) {
+       this.active = active;
+       this.status = status;
+       this.message = message;
+   }
+
+   public Error() {
+   }
+
+   public void printInfo() {
+       System.out.println("active = " + active);
+       System.out.println("status = " + status);
+       System.out.println("message = " + message);
+   }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 10, "reloading");
+        Error error1 = new Error();
+        error.printInfo();
+        error1.printInfo();
+    }
+
+}
