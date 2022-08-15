177f698082152db6041dbe3cee757d2523d72d94
finansist138 <103438132+finansist138@users.noreply.github.com>
2022-08-15 21:02:24 +0300

    8. Что такое валидация? [#246864]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -47,25 +47,23 @@ public class Tracker {
     }
 
     public boolean replace(int id, Item item) {
-        boolean replace = false;
         int index = indexOf(id);
-        if (index != -1) {
+        boolean rsl = index != -1;
+        if (rsl) {
             item.setId(id);
             items[index] = item;
-            replace = true;
         }
-        return replace;
+        return rsl;
     }
 
     public boolean delete(int id) {
         int index = indexOf(id);
-        if (index != -1) {
-            items[index] = null;
+        boolean rsl = index != -1;
+        if (rsl) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
-            return true;
         }
-        return false;
+        return rsl;
     }
 }
