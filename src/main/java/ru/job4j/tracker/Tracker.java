fa4dadba29d0eb46dcd4975a6f35741aad67464a
finansist138 <103438132+finansist138@users.noreply.github.com>
2022-08-17 09:06:31 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #332631]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -59,7 +59,6 @@ public class Tracker {
     public boolean delete(int id) {
         int index = indexOf(id);
         boolean rsl = index != -1;
-
         if (rsl) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
