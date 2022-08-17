c1e9f7a84fcf00764550760b49f511b799f30243
finansist138 <103438132+finansist138@users.noreply.github.com>
2022-08-17 10:16:26 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,12 +1,8 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String result = item.getCreated().format(formatter);
-        System.out.println(result);
+        System.out.println(item);
     }
 }
