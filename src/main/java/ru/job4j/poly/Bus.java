2d27e409efc7f601693d327a17d62f75dea59fa3
finansist138 <103438132+finansist138@users.noreply.github.com>
2022-08-30 09:12:42 +0300

    3. Интерфейс Input [#181092 #337444]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -4,12 +4,13 @@ public class Bus implements Transport {
 
     @Override
     public void drive() {
-
+        System.out.println("График движения транспорта: понедельник - пятница с 9.00 до 21.00");
     }
 
     @Override
     public void numberOfPass(int numberOfPass) {
-
+        System.out.println("Максимальная вместимость пассажиров"
+                + " в данный автобус: " + numberOfPass);
     }
 
     @Override
