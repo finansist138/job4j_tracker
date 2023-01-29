c7d5a20a9592c714c75da42f3e293ca6638eaf3c
finansist138 <103438132+finansist138@users.noreply.github.com>
2023-01-30 00:30:06 +0300

    2. Идентичные тексты [#212655]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -5,9 +5,9 @@ import java.util.HashSet;
 public class UniqueText {
     public boolean isEquals(String originText, String duplicateText) {
         boolean rsl = true;
-        String [] origin = originText.split("");
-        String [] text = duplicateText.split("");
-        HashSet<String > check = new HashSet<>();
+        String[] origin = originText.split("");
+        String[] text = duplicateText.split("");
+        HashSet<String> check = new HashSet<>();
         for (String orig : origin) {
             check.add(orig);
         }
