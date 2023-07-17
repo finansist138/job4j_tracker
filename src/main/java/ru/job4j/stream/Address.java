73136406c384fb680f9999dcf2bbfb74dfff1122
finansist138 <103438132+finansist138@users.noreply.github.com>
2023-07-17 13:50:23 +0500

    7. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Address.java
+++ src/main/java/ru/job4j/stream/Address.java
@@ -29,12 +29,16 @@ public class Address {
         Address address = (Address) o;
         return home == address.home
                 && apartment == address.apartment
-                && Objects.equals(city, address.city)
+                && Objects.equals(getCity(), address.getCity())
                 && Objects.equals(street, address.street);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(city, street, home, apartment);
+        return Objects.hash(getCity(), street, home, apartment);
+    }
+
+    public String getCity() {
+        return city;
     }
 }
